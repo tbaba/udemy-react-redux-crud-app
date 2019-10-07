51e21650df53bffe8750a06c005d47cc735eb60f
Tatsuro Baba <harakirisoul@gmail.com>
2019-10-07 21:58:06 +0900

    Functional Component and Class Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <label htmlFor="text-input">らべる</label>
-        <input id="text-input" type="text" onChange={(e) => {console.log(e.target.value)}} />
-      </>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <>
+//         <label htmlFor="text-input">らべる</label>
+//         <input id="text-input" type="text" onChange={(e) => {console.log(e.target.value)}} />
+//       </>
+//     );
+//   }
+// } 
+
+const App = () => {
+  return <div><Cat /></div>;
+};
+
+const Cat = () => {
+  return <div>Meow!</div>
+};
 
 export default App;
