c3124378790e2a37fe990059f3e92a8c181da9d4
Tatsuro Baba <harakirisoul@gmail.com>
2020-05-08 17:41:51 +0900

    Introduce react-router, and render the blank new page

---
+++ src/components/events_new.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// import { postEvent } from '../actions';
+
+class EventsNew extends Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+// const mapDispatchToProps = ({ postEvent });
+
+export default connect(null, null)(EventsNew);
